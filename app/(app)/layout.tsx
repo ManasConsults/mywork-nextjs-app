6cf987e76996df7f10288fac47e2c0a71145be50
Manas Mallick <manasm.dev@gmail.com>
2026-02-27 16:10:26 +1000

    updated layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -14,9 +14,9 @@ export default async function AppLayout({ children }: { children: ReactNode }): 
     <div className="flex min-h-screen bg-zinc-50 dark:bg-zinc-950">
       <Sidebar user={session.user} />
       <div className="flex flex-1 flex-col">
-        <header className="flex h-12 shrink-0 items-center justify-end border-b border-zinc-200 bg-white px-4 dark:border-zinc-800 dark:bg-zinc-900">
+        <div className="flex h-12 shrink-0 items-center justify-end px-4">
           <ThemeToggle />
-        </header>
+        </div>
         <main className="flex-1 overflow-y-auto p-6">{children}</main>
       </div>
     </div>
